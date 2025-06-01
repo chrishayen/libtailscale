28a35e42b6f76fec2fb174be1ada09c0d0f2bae9
Chris Hayen <chayen@gmail.com>
2025-06-01 18:38:14 -0400

    added comment

--- rust/tsnet/src/lib.rs
+++ rust/tsnet/src/lib.rs
@@ -289,6 +289,7 @@ impl TSNet {
         Ok(conn_out)
     }
 
+    /// Closes a file descriptor.
     pub fn close_fd(fd: i32) -> Result<(), String> {
         let result = unsafe { libc::close(fd) };
         if result != 0 {
